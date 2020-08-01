076c1e41409008be99c1d1e48ad32f9704fe1f32
Alex Smith <smithavt14@gmail.com>
2020-08-01 16:35:37 +0800

    Student updated version

--- pages/show/show.js
+++ pages/show/show.js
@@ -20,22 +20,43 @@ Page({
     })
   },
   submitOrder: function (e) {
-    console.log(e)
     let meal_id = e.currentTarget.dataset.id;
-    let Order = new wx.BaaS.TableObject('order');
-    let user = wx.BaaS.auth.getCurrentUser()
-    let data = {meal_id, user_id: user.id};
-    let MyRecord = Order.create()
-    MyRecord.set(data).save().then(res => {
-      console.log(res)
-      wx.reLaunch({
-        url: '../user/user',
-      })
-    });
+    let points = e.currentTarget.dataset.points
+    wx.BaaS.auth.getCurrentUser().then(user => {
+      let user_points = user.get('points')
+      if (points > 0 || user_points > points) {
+        let data = {meal_id, user_id: user.id, points};
+        let Order = new wx.BaaS.TableObject('order');
+        
+        let MyRecord = Order.create()
 
+        MyRecord.set(data).save().then(res => {
+          let order = res.data;
+          this.updatePoints(user, order);
+          
+          wx.reLaunch({
+            url: '../user/user',
+          })
+        });
+      } else {
+        wx.showModal({
+          title: 'Get the hell outta here u cheap font!',
+        })
+      }
+    }); 
   },
 
-  
+  updatePoints: function (user, order) {
+    // add points from new order to user's total points
+    // 1.Fetch user points
+    let points = user.get("points");
+    // 2.Fetch order points
+    // 3.Add user points and order points together
+    points = points + order.points;
+    // 4.Update user entry with new points
+    user.set("points", points).update();
+
+  },
 
   onLoad: function (options) {
     let id = options.id;
